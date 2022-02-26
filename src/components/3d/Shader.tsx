d5f33bfb38db1600bfe809bd1c7136231a18d937
Hiren Umradia <hiren@zess.co>
2022-02-26 17:10:04 +0000

    Added starts

--- src/components/3d/Shader.tsx
+++ src/components/3d/Shader.tsx
@@ -1,14 +1,43 @@
 import * as THREE from "three";
 import { Heading, Box, Main } from "grommet";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Component, useRef, useState } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Component, useMemo, useRef, useState, Suspense } from "react";
+import { BufferAttribute, BufferGeometry, TextureLoader } from "three";
 
 export default function Shader(props: JSX.IntrinsicElements["mesh"]) {
   const meshRef = useRef<THREE.Mesh>(null!);
-  const pointsRef = useRef<THREE.Mesh>(null!);
+
+  const vertices = [];
+
+  for (let i = 0; i < 10000; i++) {
+    const x = THREE.MathUtils.randFloatSpread(2000);
+    const y = THREE.MathUtils.randFloatSpread(2000);
+    const z = THREE.MathUtils.randFloatSpread(2000);
+
+    vertices.push(x, y, z);
+  }
+
+  const pointsRef = useRef<THREE.BufferGeometry>(null!);
+
+  const starSprite = useLoader(TextureLoader, "/starSprite.png");
+
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   useFrame((state, delta) => (meshRef.current.rotation.x += 0.01));
+
+  const starDistance: number = 250;
+  const starQuantity: number = 1250;
+
+  const [positions] = useMemo(() => {
+    let positions = [];
+    for (let i = 0; i < starQuantity; i++) {
+      positions.push(starDistance - Math.random() * starDistance * 2);
+      positions.push(starDistance - Math.random() * starDistance * 2);
+      positions.push(starDistance - Math.random() * starDistance * 2);
+    }
+    return [new Float32Array(positions)];
+  }, [starQuantity]);
+
   return (
     <>
       <mesh
@@ -20,8 +49,26 @@ export default function Shader(props: JSX.IntrinsicElements["mesh"]) {
         onPointerOut={(event) => hover(false)}
       >
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+        <meshStandardMaterial color={hovered ? "pink" : "orange"} />
       </mesh>
+      <points>
+        <bufferGeometry attach="geometry" ref={pointsRef}>
+          <bufferAttribute
+            attachObject={["attributes", "position"]}
+            count={positions.length / 3}
+            array={positions}
+            itemSize={3}
+          ></bufferAttribute>
+        </bufferGeometry>
+        <pointsMaterial
+          attach="material"
+          color={"white"}
+          map={starSprite}
+          alphaTest={0.5}
+          transparent={true}
+          sizeAttenuation={true}
+        />
+      </points>
       {/* <bufferGeometry>
         <vector3 args={[0, 0, 0]} />
       </bufferGeometry> */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,51 @@
 import { Heading, Box, Main } from "grommet";
 import Head from "next/head";
 import Shader from "../components/3d/Shader";
-import { Component } from "react";
+import { Component, Suspense } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import {
   attributes,
   react as HomeContent,
 } from "../../_collections/posts/oxie.md";
+import { FC } from "react";
 import { Euler } from "three";
 
-export default class Home extends Component {
-  render() {
-    let { title, cats } = attributes;
-    const cameraRotation = new Euler(Math.PI / 2, 0, 1);
-    return (
-      <>
-        <Main pad="large">
-          <Heading size="xxxlarge">{title}</Heading>
-          <HomeContent />
-          <Box>
-            {cats.map((cat, k) => (
-              <Box key={k}>
-                <Heading size="xlarge">{cat.name}</Heading>
-                <p>{cat.description}</p>
-              </Box>
-            ))}
-            <Canvas>
+const Home: FC<unknown> = () => {
+  let { title, cats } = attributes;
+  const cameraRotation = new Euler(Math.PI / 2, 0, 1);
+  return (
+    <>
+      <Main pad="large">
+        <Heading size="xxxlarge">{title}</Heading>
+        <HomeContent />
+        <Box style={{ zIndex: 1 }}>
+          {cats.map((cat, k) => (
+            <Box key={k}>
+              <Heading size="xlarge">{cat.name}</Heading>
+              <p>{cat.description}</p>
+            </Box>
+          ))}
+        </Box>
+        <Box fill={true} style={{ position: "absolute" }}>
+          <Canvas>
+            <Suspense fallback={null}>
               <perspectiveCamera
                 position={1}
                 rotation={cameraRotation}
                 fov={60}
                 near={1}
-                far={1000}
+                far={2000}
               />
               <ambientLight />
               <pointLight position={[10, 10, 10]} />
               <Shader position={[-1.2, 0, 0]}></Shader>
               <Shader position={[1.2, 0, 0]}></Shader>
-            </Canvas>
-          </Box>
-        </Main>
-      </>
-    );
-  }
-}
+            </Suspense>
+          </Canvas>
+        </Box>
+      </Main>
+    </>
+  );
+};
+
+export default Home;
